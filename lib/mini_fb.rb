3919926b0794f26244780fec014e532d117b6c91
buffpojken <daniel@sykewarrior.com>
2011-10-03 11:56:37 +0200

    tweak

--- lib/mini_fb.rb
+++ lib/mini_fb.rb
@@ -344,7 +344,7 @@ module MiniFB
     def MiniFB.parse_oauth2_cookie_information(app_id, app_secret, cookies)
       return nil if cookies["fbsr_#{app_id}"].nil?
       sig, payload = cookies["fbsr_#{app_id}"].split(".")
-      data = JSON.parse(Base64.decode64(payload)+'"}') 
+      data = JSON.parse(Base64.decode64(payload)+'}') 
       begin
         e = RestClient.get URI.escape("https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=&client_secret=#{app_secret}&code=#{data['code']}&response_type=token")
       rescue Exception => e
